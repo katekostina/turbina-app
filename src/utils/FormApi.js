86cde01d32f3e2122d734806748555eaf6fbbd14
Andrey <ScootalooRainbow@yandex.ru>
2020-11-01 16:40:29 +0900

    develop fixed

--- src/utils/FormApi.js
+++ src/utils/FormApi.js
@@ -1,12 +1,10 @@
-// Заготовка АПИ для формы
-
 class FormApi {
     constructor({ baseUrl, headers }) {
         this.baseUrl = baseUrl;
         this.headers = headers;
     }
 }
-export const apiProfile = new Api({
+export const apiProfile = new FormApi({
     baseUrl: '',
     headers: {
         authorization: '',
