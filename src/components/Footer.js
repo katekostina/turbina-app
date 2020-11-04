faa7f26e1e7e83ab481a15fd4583f7deefc0daed
Sergei Kompanietc <delightvlg@gmail.com>
2020-11-04 22:07:03 +0300

    Added footer. Needs to be mount in Page component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../blocks/footer/footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__copyrights">&#169; Маршак, 2020.</p>
+      <p className="footer__made-by">Сделано студентами&nbsp;
+        <a className="footer__link"
+           href="https://praktikum.yandex.ru/"
+           rel="noreferrer" target="_blank">Яндекс.Практикум</a></p>
+    </footer>
+  )
+}
+
+export default Footer;
