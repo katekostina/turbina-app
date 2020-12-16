50c351a869f3fd7ec00215ce22df62ca1a11a10b
Kate Kostina <katekostina@yahoo.com>
2020-12-16 16:47:16 +0300

    Refactor Article: delete unneeded container

--- src/components/ContentBox.js
+++ src/components/ContentBox.js
@@ -1,10 +1,40 @@
 import React from "react";
-import Articles from "./Articles.js";
 import Form from "./Form.js";
+import { articlesContent } from "../utils/texts.js";
+
 
 const ContentBox = () =>
     <section className="content-box">
-      <Articles />
+      {articlesContent && (
+          articlesContent.map((article) => (
+            <article className="article" key={article.id}>
+              {/* Если есть заголовок — рендерим его */}
+              {article.heading && (
+                <h2 className="article__heading">{article.heading}</h2>
+              )}
+
+              {/* Если есть абзацы текста, то отрендерить их  */}
+              {article.paragraphs && (
+                  article.paragraphs.map((paragraph) => (
+                    <p className="article__paragraph" key={paragraph.id}>
+                      {paragraph.text}
+                    </p>
+                  ))
+              )}
+
+              {/* Если есть список, то отрендерить его */}
+              {article.list && (
+                <ul className="article__list">
+                  {article.list.map((item) => (
+                    <li className="article__list-item" key={item.id}>
+                      {item.text}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </article>
+          ))
+      )}
       <Form />
     </section>
 
